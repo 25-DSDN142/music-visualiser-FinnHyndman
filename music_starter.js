333d41c34c7272002a22d33dda33e0a8e663c383
FinnHyndman <finnhyndman@gmail.com>
2025-08-12 12:14:26 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -4,23 +4,57 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
-  
-  
+  background(0)
+  colorMode(HSB,100);
 
-   let cirleScale = drum;
-   let colourValueA = map (vocal,0,100,0,255)
-   let colourValueB = bass
-   let colourValueC = other
 
-   //let backgroundColA = 300-colourValueA
-   //let backgroundColB = 100-colourValueB
-   //let backgroundColC = 100-colourValueC
 
- //  background(backgroundColA,backgroundColB,backgroundColC)
-background(0)
+stroke(255)
+strokeWeight(5)
 
-// changes 
-fill(colourValueA, colourValueB, colourValueC);
-circle (320,240,cirleScale*3)
+let vocalMap = map(vocal,0,100,30,90)
+let bassMap = map(bass,0,100,30,90)
 
-}
+
+let circleScale = vocalMap
+let circleHue = vocalMap
+let circleMove = bassMap
+
+
+noStroke()
+fill(60,circleHue,100)
+
+for(let i=0; i<3; i++){
+let circleGap = i*circleMove +50
+circle (320,240+circleGap,circleScale)
+
+for(let i=0; i<3; i++){
+let circleGap = i*circleMove +50
+circle (320,240-circleGap,circleScale)
+
+for(let i=0; i<3; i++){
+let circleGap = i*circleMove+50
+circle (320-circleGap,240,circleScale)
+
+for(let i=0; i<3; i++){
+let circleGap = i*circleMove+50
+circle (320+circleGap,240,circleScale)
+
+for(let i=0; i<2; i++){
+let circleGap = i*circleMove+55
+circle (320+circleGap,240+circleGap,circleScale)
+
+for(let i=0; i<2; i++){
+let circleGap = i*circleMove+55
+circle (320+circleGap,240-circleGap,circleScale)
+
+for(let i=0; i<2; i++){
+let circleGap = i*circleMove+55
+circle (320-circleGap,240-circleGap,circleScale)
+
+for(let i=0; i<2; i++){
+let circleGap = i*circleMove+55
+circle (320-circleGap,240+circleGap,circleScale)
+
+
+}}}}}}}}}
