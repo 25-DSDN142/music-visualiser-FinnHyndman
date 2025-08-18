8f0525b673fc5005c73acb6ff9376f7bb6dce1b4
FinnHyndman <finnhyndman@gmail.com>
2025-08-18 13:58:28 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,60 +1,45 @@
 
+
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
   background(0)
-  colorMode(HSB,100);
+
+// canvas centre
+let x = 320
+let y = 240
+
+let t = vocal / 100; //scaling vocal values to be between 0 - 1
+//lerp each color channel
+let red = lerp(255, 0, t);   // orange to blue
+let green = lerp(136, 200, t); // orange to blue
+let blue = lerp(0, 255, t);   // orange to blue
+
+//scaling to make vocals scale to circle size
+let vocalScale = map(vocal, 0, 100, 0, 300);
+
+ // blob with fading edges using a for loop
+  for (let size = vocalScale; size > 0; size -= 5) {
+    let fade = map(size, 0, vocalScale, 0, 80); // fade edges
+
+    fill(red, green, blue, fade);              // cyan glow
+    noStroke();
+    ellipse(x, y, size, size);
+    
+
+  //grain overlay
+  let grainAmount = map(bass, 0, 100, 800, 1000); // scale bass to quantity of cirlce drawn for grain effect.
+  fill(255, 255, 255, 15); // semi-transparent white
+  noStroke();
+  for (let i = 0; i < grainAmount; i++) {
+    ellipse(random(width), random(height), 1.5, 1.5);
+  }
 
 
+  fill(255)
+  circle (x,y,420)
 
-stroke(255)
-strokeWeight(5)
-
-let vocalMap = map(vocal,0,100,30,90)
-let bassMap = map(bass,0,100,30,90)
-
-
-let circleScale = vocalMap
-let circleHue = vocalMap
-let circleMove = bassMap
-
-
-noStroke()
-fill(60,circleHue,100)
-
-for(let i=0; i<3; i++){
-let circleGap = i*circleMove +50
-circle (320,240+circleGap,circleScale)
-
-for(let i=0; i<3; i++){
-let circleGap = i*circleMove +50
-circle (320,240-circleGap,circleScale)
-
-for(let i=0; i<3; i++){
-let circleGap = i*circleMove+50
-circle (320-circleGap,240,circleScale)
-
-for(let i=0; i<3; i++){
-let circleGap = i*circleMove+50
-circle (320+circleGap,240,circleScale)
-
-for(let i=0; i<2; i++){
-let circleGap = i*circleMove+55
-circle (320+circleGap,240+circleGap,circleScale)
-
-for(let i=0; i<2; i++){
-let circleGap = i*circleMove+55
-circle (320+circleGap,240-circleGap,circleScale)
-
-for(let i=0; i<2; i++){
-let circleGap = i*circleMove+55
-circle (320-circleGap,240-circleGap,circleScale)
-
-for(let i=0; i<2; i++){
-let circleGap = i*circleMove+55
-circle (320-circleGap,240+circleGap,circleScale)
-
-
-}}}}}}}}}
+  
+  }}
